cd2cb2598cc3e637278356af87de281efa047e6e
Camilo Sánchez <info@caxtor.co>
2023-11-10 04:13:24 -0500

    Thor Templates

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# NopioScaffold
-
-This is the resulting gem of this tutorial: https://www.nopio.com/blog/creating-scaffold-generator-rails/.
-
-## Installation
-
-Add this line to your application's Gemfile:
-
-```ruby
-gem 'nopio_scaffold', git: 'https://github.com/nopio/nopio-scaffold.git'
-```
-
-And then execute:
-
-    $ bundle install
-
-## Usage
-
-```bash
-rails g nopio_scaffold:controller [MODEL_NAME]
-```
-
-## License
-
-The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
-
-rails g nopio_scaffold:controller Trial
+rails g model books user_id:integer title:string author:string year:integer
+rake db:migrate
+rails g nopio_scaffold:controller books
--- lib/generators/nopio_scaffold/templates/controller.rb
+++ lib/generators/nopio_scaffold/templates/controller.rb
@@ -1,45 +1,88 @@
+# frozen_string_literal: true
+
 class <%= controller_class_name %>Controller < ApplicationController
-  before_action :authenticate_user!
-  load_and_authorize_resource
+  before_action :authenticate_admin!
+  theme 'dashboard'
+  before_action :set_backoffice
+  before_action :set_breadcrumb
+  before_action :set_<%= singular_name %>, only: %i[show edit update destroy hard_destroy]
 
   def index
+    add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>')
+    @<%= plural_table_name %> = <%= class_name %>.order(created_at: :desc)
+    @pagy, @<%= plural_table_name %> = pagy(@<%= plural_table_name %>)
   end
-<% if show_action? -%>
-
+  <% if show_action? -%>
   def show
+    add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>'), <%= index_helper %>_path
   end
-<% end -%>
-
+  <% end -%>
   def new
-  end
-
-  def create
-    if @<%= singular_name %>.save
-      redirect_to @<%= singular_name %>, notice: '<%= class_name.underscore.humanize %> was successfully created.'
-    else
-      render :new
-    end
+    add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>'), <%= index_helper %>_path
+    add_breadcrumb I18n.t('backoffice.buttons.new_<%= singular_table_name %>')
+    @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
   end
 
   def edit
+    add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>'), <%= index_helper %>_path
+    add_breadcrumb I18n.t('backoffice.buttons.edit_<%= singular_table_name %>')
+  end
+
+  def create
+    @<%= singular_table_name %> = <%= orm_class.build(class_name, "#{singular_table_name}_params") %>
+    respond_to do |format|
+      if @<%= orm_instance.save %>
+        format.html { redirect_to @<%= singular_table_name %>, notice: t('<%= table_name %>.was_created') }
+      else
+        add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>'), <%= index_helper %>_path
+        add_breadcrumb I18n.t('backoffice.buttons.new_<%= singular_table_name %>')
+        format.html { render :new }
+      end
+    end
   end
 
   def update
-    if @<%= singular_name %>.update(<%= singular_name %>_params)
-      redirect_to @<%= singular_name %>, notice: '<%= class_name.underscore.humanize %> was successfully updated.'
-    else
-      render :edit
+    respond_to do |format|
+      if @<%= singular_table_name %>.update(<%= singular_table_name %>_params)
+        format.html { redirect_to @<%= singular_table_name %>, notice: t('<%= table_name %>.was_updated') }
+      else
+        add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>'), <%= index_helper %>_path
+        add_breadcrumb I18n.t('backoffice.buttons.edit_<%= singular_table_name %>')
+        format.html { render :edit }
+      end
+      format.json { respond_with_bip(@<%= singular_table_name %>) }
     end
   end
 
   def destroy
-    @<%= singular_name %>.destroy
-    redirect_to <%= plural_name %>_url, notice: '<%= class_name.underscore.humanize %> was successfully destroyed.'
+    @<%= singular_table_name %>.destroy
+    respond_to do |format|
+      format.html { redirect_to <%= index_helper %>_url, notice: t('<%= table_name %>.was_deleted') }
+    end
+  end
+
+  def hard_destroy
+    @<%= singular_table_name %>.really_destroy!
+    respond_to do |format|
+      format.html { redirect_to <%= index_helper %>_url, notice: t('<%= table_name %>.was_deleted') }
+    end
+  end
+
+  def sort
+    params[:<%= singular_table_name %>].each_with_index do |id, index|
+      <%= class_name %>.where(id:).update_all(position: index + 1)
+    end
+    head :ok
   end
 
   private
 
+  def set_<%= singular_name %>
+    @<%= singular_name %> = <%= class_name %>.where('hash_code = ? AND store_id = ?', params[:id], @store.id).first
+    redirect_if_not_found('<%= class_name %> no existe en set_<%= singular_name %>') if @<%= singular_name %>.blank?
+  end
+
   def <%= singular_name %>_params
-    params.require(:<%= singular_name %>).permit(<%= editable_attributes.map { |a| a.name.dup.prepend(':') }.join(', ') %>)
+    params.require(:<%= singular_name %>).permit(<%= attributes.map { |a| a.name.prepend(':') }.join(', ') %>)
   end
 end
