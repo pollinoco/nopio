eba9b328d6ecdd0e30993eb54c266f787613958f
Camilo Sánchez <info@caxtor.co>
2023-11-10 06:51:08 -0500

    upload

--- lib/generators/nopio_scaffold/templates/controller.rb
+++ lib/generators/nopio_scaffold/templates/controller.rb
@@ -6,34 +6,34 @@ class <%= controller_class_name %>Controller < ApplicationController
   before_action :set_<%= singular_name %>, only: %i[show edit update destroy hard_destroy]
 
   def index
-    add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>')
-    @<%= plural_table_name %> = <%= class_name %>.order(position: :asc)
-    @pagy, @<%= plural_table_name %> = pagy(@<%= plural_table_name %>)
+    add_breadcrumb I18n.t('backoffice.<%= plural_name %>')
+    @<%= plural_name %> = <%= class_name %>.order(position: :asc)
+    @pagy, @<%= plural_name %> = pagy(@<%= plural_name %>)
   end
   <% if show_action? -%>
   def show
-    add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>'), <%= index_helper %>_path
+    add_breadcrumb I18n.t('backoffice.<%= plural_name %>'), <%= plural_name %>_path
   end
   <% end -%>
   def new
-    add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>'), <%= index_helper %>_path
-    add_breadcrumb I18n.t('backoffice.buttons.new_<%= singular_table_name %>')
-    @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
+    add_breadcrumb I18n.t('backoffice.<%= plural_name %>'), <%= plural_name %>_path
+    add_breadcrumb I18n.t('backoffice.buttons.new_<%= singular_name %>')
+    @<%= singular_name %> = <%= orm_class.build(class_name) %>
   end
 
   def edit
-    add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>'), <%= index_helper %>_path
-    add_breadcrumb I18n.t('backoffice.buttons.edit_<%= singular_table_name %>')
+    add_breadcrumb I18n.t('backoffice.<%= plural_name %>'), <%= plural_name %>_path
+    add_breadcrumb I18n.t('backoffice.buttons.edit_<%= singular_name %>')
   end
 
   def create
-    @<%= singular_table_name %> = <%= orm_class.build(class_name, "#{singular_table_name}_params") %>
+    @<%= singular_name %> = <%= orm_class.build(class_name, "#{singular_name}_params") %>
     respond_to do |format|
       if @<%= orm_instance.save %>
-        format.html { redirect_to @<%= singular_table_name %>, notice: t('<%= table_name %>.was_created') }
+        format.html { redirect_to @<%= singular_name %>, notice: t('<%= class_name.underscore.humanize %>.was_created') }
       else
-        add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>'), <%= index_helper %>_path
-        add_breadcrumb I18n.t('backoffice.buttons.new_<%= singular_table_name %>')
+        add_breadcrumb I18n.t('backoffice.<%= plural_name %>'), <%= plural_name %>_path
+        add_breadcrumb I18n.t('backoffice.buttons.new_<%= singular_name %>')
         format.html { render :new }
       end
     end
@@ -41,33 +41,33 @@ class <%= controller_class_name %>Controller < ApplicationController
 
   def update
     respond_to do |format|
-      if @<%= singular_table_name %>.update(<%= singular_table_name %>_params)
-        format.html { redirect_to @<%= singular_table_name %>, notice: t('<%= table_name %>.was_updated') }
+      if @<%= singular_name %>.update(<%= singular_name %>_params)
+        format.html { redirect_to @<%= singular_name %>, notice: t('<%= class_name.underscore.humanize %>.was_updated') }
       else
-        add_breadcrumb I18n.t('backoffice.<%= plural_table_name %>'), <%= index_helper %>_path
-        add_breadcrumb I18n.t('backoffice.buttons.edit_<%= singular_table_name %>')
+        add_breadcrumb I18n.t('backoffice.<%= plural_name %>'), <%= plural_name %>_path
+        add_breadcrumb I18n.t('backoffice.buttons.edit_<%= singular_name %>')
         format.html { render :edit }
       end
-      format.json { respond_with_bip(@<%= singular_table_name %>) }
+      format.json { respond_with_bip(@<%= singular_name %>) }
     end
   end
 
   def destroy
-    @<%= singular_table_name %>.destroy
+    @<%= singular_name %>.destroy
     respond_to do |format|
-      format.html { redirect_to <%= index_helper %>_url, notice: t('<%= table_name %>.was_deleted') }
+      format.html { redirect_to <%= plural_name %>_url, notice: t('<%= class_name.underscore.humanize %>.was_deleted') }
     end
   end
 
   def hard_destroy
-    @<%= singular_table_name %>.really_destroy!
+    @<%= singular_name %>.really_destroy!
     respond_to do |format|
-      format.html { redirect_to <%= index_helper %>_url, notice: t('<%= table_name %>.was_deleted') }
+      format.html { redirect_to <%= plural_name %>_url, notice: t('<%= class_name.underscore.humanize %>.was_deleted') }
     end
   end
 
   def sort
-    params[:<%= singular_table_name %>].each_with_index do |id, index|
+    params[:<%= singular_name %>].each_with_index do |id, index|
       <%= class_name %>.where(id:).update_all(position: index + 1)
     end
     head :ok
